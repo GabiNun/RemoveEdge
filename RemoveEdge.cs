1c0a0c79caee3abe6b24961842b68f2ed2b86a4e
Gabi <182965942+GabiNun@users.noreply.github.com>
2025-10-17 19:46:53 +0300

    Update RemoveEdge.cs

--- RemoveEdge.cs
+++ RemoveEdge.cs
@@ -13,6 +13,7 @@ class Program
         foreach (string dir in Directory.GetDirectories(@"C:\Program Files (x86)\Microsoft\Edge\Application")) 
         {
             Process.Start(Path.Combine(dir, "Installer", "setup.exe"), "--uninstall --system-level --force-uninstall --delete-profile");
+            process.WaitForExit();
             break;
         }
         Directory.Delete(folderPath, true);
